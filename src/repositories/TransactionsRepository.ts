2ac639318e3a47f45e1fe2db98432b0c20586603
Leonardo S. Matos <matos_leo95@live.com>
2020-05-02 00:27:31 -0300

    Add soluçao de balance melhorada (comentado)

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -41,6 +41,34 @@ class TransactionsRepository {
     };
   }
 
+  /*
+    Solução melhorada
+  */
+
+  // public getBalance(): Balance {
+  //   // TODO
+  //   const { income, outcome } = this.transactions.reduce(
+  //     (accumulator, transaction) => {
+  //       switch (transaction.type) {
+  //         case 'income':
+  //           accumulator.income += transaction.value;
+  //           break;
+  //         case 'outcome':
+  //           accumulator.outcome += transaction.value;
+  //           break;
+  //         default:
+  //           break;
+  //       }
+
+  //       return accumulator;
+  //     },
+  //     { income: 0, outcome: 0 },
+  //   );
+
+  //   const total = income - outcome;
+  //   return { income, outcome, total };
+  // }
+
   public create({ title, value, type }: CreateTransactionDTO): Transaction {
     // TODO
     const transaction = new Transaction({ title, value, type });
